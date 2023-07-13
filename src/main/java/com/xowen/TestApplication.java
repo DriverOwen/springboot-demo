37f52e71e574320a61d3d81466ecc656fc230d9f
DriverOwen <bug-owen@qq.com>
2023-07-13 16:18:38 +0800

    update

--- src/main/java/com/xowen/TestApplication.java
+++ src/main/java/com/xowen/TestApplication.java
@@ -1,13 +1,27 @@
 package com.xowen;
 
+import com.example.EnableHeaderConfig;
+import com.example.HeaderConfig;
+import com.example.HeaderParser;
+import com.example.TokenParser;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
+//@Import({TokenParser.class}) // 导入普通类，交给ioc管理
+//@Import({HeaderGenerator.class})
+//@Import({HeaderConfig.class})
+//@Import({MyImportSelector.class}) // 导入importSelector接口实现类，批量交给IOC管理
+//@ComponentScan({"com.example","com.xowen"})
+// No auto configuration classes found in META-INF/spring.factories nor in META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports. If you are using a custom packaging, make sure that file is correct.
+@EnableHeaderConfig
 @ServletComponentScan  // 在spring项目中启用servlet组件
 @SpringBootApplication
 public class TestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
--- src/test/java/com/xowen/BeanIoc.java
+++ src/test/java/com/xowen/BeanIoc.java
@@ -1,17 +1,29 @@
 package com.xowen;
 
+import com.example.HeaderParser;
+import com.example.TokenParser;
+import com.example.HeaderGenerator;
+
+import com.xowen.starterDemo.JwtUtilsStarter;
 import com.xowen.utils.JwtUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 public class BeanIoc {
     @Autowired
     private ApplicationContext applicationContext; // IOC容器对象
 
-    @Test
+    // 没有手动注册到IOC容器，spring 自动注册的
+    @Autowired
+    private JwtUtilsStarter jwtUtilsStarter;
+
+    @Test // bean的主动获取方式
     public void testGetBean(){
         // 根据bean的名称获取 注意是小写
         JwtUtils jwtUtils1 = (JwtUtils) applicationContext.getBean("jwtUtils");
@@ -24,4 +36,34 @@ public class BeanIoc {
         System.out.println(jwtUtils3);
     }
 
+
+    @Test // 调用spring启动时自动配置的bean  private Gson gson @Autowire
+    public void testAutoBean(){
+
+
+    }
+
+    // 测试第三方依赖
+    @Test
+    public void testTokenParser(){
+        System.out.println(applicationContext.getBean(TokenParser.class));
+    }
+    @Test
+    public void testHeaderParser(){
+        System.out.println(applicationContext.getBean(HeaderParser.class));
+    }
+    @Test
+    public void testHeaderGenParser(){
+        System.out.println(applicationContext.getBean(HeaderGenerator.class));
+    }
+
+    // 测试 starterDemo 自动注入
+    @Test
+    public void testStarterDemo(){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", "test");
+        claims.put("username", "xowen");
+        String jwt = jwtUtilsStarter.generateJwt(claims);
+        System.out.println(jwt);
+    }
 }
